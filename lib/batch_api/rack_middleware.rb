c98e467dcfb5e857f06ee6f515ea51b15ff7e9f2
Alex Neyman <leshaneyman@gmail.com>
2014-10-24 18:45:55 +0400

    make some helpers class-level 4 external usage

--- lib/batch_api/rack_middleware.rb
+++ lib/batch_api/rack_middleware.rb
@@ -1,5 +1,29 @@
 module BatchApi
   class RackMiddleware
+
+    class << self
+      def content_type
+        { "Content-Type" => "application/json" }
+      end
+
+      def allow
+        { "Allow" => BatchApi.config.verb.to_s.upcase }
+      end
+
+      def batch_endpoint?(env)
+        env["PATH_INFO"] == BatchApi.config.endpoint
+      end
+
+      def batch_method?(env)
+        env["REQUEST_METHOD"] == BatchApi.config.verb.to_s.upcase
+      end
+
+      def batch_request?(env)
+        batch_endpoint?(env) && batch_method?(env)
+      end
+    end
+
+
     def initialize(app, &block)
       @app = app
       yield BatchApi.config if block
@@ -7,7 +31,7 @@ module BatchApi
 
     def call(env)
       case
-      when batch_endpoint?(env) && batch_method?(env)
+      when batch_request?(env)
         begin
           request = request_klass.new(env)
           result = BatchApi::Processor.new(request, @app).execute!
@@ -22,23 +46,12 @@ module BatchApi
       end
     end
 
-    def self.content_type
-      {"Content-Type" => "application/json"}
-    end
-
-    def self.allow
-      {"Allow" => BatchApi.config.verb.to_s.upcase}
-    end
 
     private
 
-    def batch_endpoint?(env)
-      env["PATH_INFO"] == BatchApi.config.endpoint
-    end
-
-    def batch_method?(env)
-      env["REQUEST_METHOD"] == BatchApi.config.verb.to_s.upcase
-    end
+    def batch_endpoint?(env) self.class.batch_endpoint?(env) end
+    def batch_method?(env) self.class.batch_method?(env) end
+    def batch_request?(env) self.class.batch_request?(env) end
 
     def options?(env)
       env["REQUEST_METHOD"] == 'OPTIONS'
